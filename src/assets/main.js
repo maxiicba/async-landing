10f86d5273d5497c18c749e0b6ccbd2dbb0c54ed
maxiicba <maxiargentino87@gmail.com>
2023-03-06 20:05:59 -0300

    finally

--- src/assets/main.js
+++ src/assets/main.js
@@ -52,7 +52,11 @@ async function fetchData(urlApi){
 const API =
   "https://youtube-v31.p.rapidapi.com/search?channelId=UCam8T03EOFBsNdR0thrFHdQ&part=snippet%2Cid&order=date&maxResults=50";
 
+const API2 = "https://youtube-v31.p.rapidapi.com/channels?part=snippet%2Cstatistics&id=UCam8T03EOFBsNdR0thrFHdQ"
+
 const content = null || document.getElementById("content");
+const textContent = null || document.getElementById("insertar");
+const image = null || document.getElementById("image");
 
 const options = {
   method: "GET",
@@ -69,9 +73,12 @@ async function fetchData(urlApi) {
 }
 
 const URLyt = "https://www.youtube.com/watch?v=";
+
 (async () => {
   try {
     const videos = await fetchData(API);
+    const channelInfo = await fetchData(API2);
+    console.log(channelInfo)
     let view = `
     ${videos.items
       .map(
@@ -96,6 +103,21 @@ const URLyt = "https://www.youtube.com/watch?v=";
       .join("")}
 
     `;
+    let view2 = `
+      <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+        <span class="block xl:inline">${channelInfo.items[0].brandingSettings.channel.title}</span>
+        <span class="block text-indigo-600 xl:inline">${channelInfo.items[0].snippet.customUrl}</span>
+      </h1>
+      <p
+        class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+        ${channelInfo.items[0].brandingSettings.channel.description}</p>
+    `;
+
+    let imageView = `<img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+    src="${channelInfo.items[0].snippet.thumbnails.high.url}" alt="">`;
+
+    image.innerHTML = imageView;
+    textContent.innerHTML = view2;
     content.innerHTML = view;
   } catch (error) {
     console.log(error);
